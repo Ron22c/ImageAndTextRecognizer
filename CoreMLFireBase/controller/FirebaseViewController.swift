1838c7f53cc4367604051797c0da047ea5051ad6
Ranajit <>
2020-09-17 21:48:32 +0530

    ui changes and permission changes

--- CoreMLFireBase/controller/FirebaseViewController.swift
+++ CoreMLFireBase/controller/FirebaseViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import Vision
 import Firebase
+import Photos
 
 class FirebaseViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet var imageViewForText: UIImageView!
-    @IBOutlet var labelText: UILabel!
+    var imageViewForText: UIImageView!
+    var labelText: UILabel!
+    var camera: UIButton!
     
     let imagePicker = UIImagePickerController()
     let vision = Vision.vision()
@@ -21,15 +23,22 @@ class FirebaseViewController: UIViewController, UIImagePickerControllerDelegate,
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white;
         textRecognizer = vision.onDeviceTextRecognizer()
+        addUiElements()
+        setFrames()
+        observeOrientation()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
         
     }
     
-    @IBAction func cameraPressed(_ sender: UIButton) {
-        present(imagePicker, animated: true, completion: nil)
+    @objc func cameraPressed(_ sender: UIButton) {
+        PHPhotoLibrary.execute(controller: self, onAccessHasBeenGranted: {
+            self.present(self.imagePicker, animated: true, completion: nil)
+        })
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -47,10 +56,53 @@ class FirebaseViewController: UIViewController, UIImagePickerControllerDelegate,
             (text, error) in
             guard error == nil, let result = text, !result.text.isEmpty else {
                    print("Not Found", [])
+                   self.labelText.text = "NO TEXT DETECTED"
                    return
             }
             print(result.text)
             self.labelText.text = result.text
         }
+    }
+    
+    func addUiElements() {
+        imageViewForText = UIImageView()
+        imageViewForText.image = UIImage(named: "welcome.jpg")
+        
+        camera = UIButton();
+        camera?.setTitle("PHOTOS", for: UIControl.State.normal);
+        camera?.addTarget(self, action: #selector(FirebaseViewController.cameraPressed), for: UIControl.Event.touchUpInside);
+        camera?.setTitleColor(UIColor.black, for: UIControl.State.normal);
+        camera?.backgroundColor=UIColor.gray;
+        
+        labelText = UILabel();
+        labelText.textAlignment = NSTextAlignment.center;
+        labelText.text = "WELCOME PLEASE CHOOSE IMAGE"
+        
+        
+        self.view.addSubview(imageViewForText);
+        self.view.addSubview(camera);
+        self.view.addSubview(labelText);
+    }
+    
+    func observeOrientation() {
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications();
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.rotation),
+                                               name: UIDevice.orientationDidChangeNotification, object: nil);
+    }
+    
+    func setFrames() {
+        camera?.frame = CGRect(x: 0, y: (self.view.bounds.size.height/10),
+                              width: self.view.bounds.size.width,
+                              height: (self.view.bounds.size.height/10));
+        imageViewForText?.frame = CGRect(x: 0, y: (2*(5+self.view.bounds.size.height/10)),
+                              width: self.view.bounds.size.width,
+                              height: (5*(self.view.bounds.size.height/10)));
+        labelText?.frame = CGRect(x: 0, y: (7*(5+self.view.bounds.size.height/10)),
+                                    width: self.view.bounds.size.width,
+                                    height: (self.view.bounds.size.height/10));
+    }
+    
+    @objc func rotation(sender: UIButton) {
+        setFrames();
     }
 }
--- CoreMLFireBase/controller/ViewController.swift
+++ CoreMLFireBase/controller/ViewController.swift
@@ -9,25 +9,36 @@
 import UIKit
 import CoreML
 import Vision
+import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let pickerController = UIImagePickerController()
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var objectName: UILabel!
+    var imageView: UIImageView!
+    var camera: UIButton!
+    var nextPage: UIButton!
+    var objectName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addUiElements()
+        setFrames()
+        observeOrientation()
         pickerController.delegate = self
         pickerController.sourceType = .photoLibrary
         pickerController.allowsEditing = false
+
     }
 
-    @IBAction func nextPage(_ sender: UIBarButtonItem) {
-        performSegue(withIdentifier: "firebase", sender: self)
+    @objc func textRecognizer(_ sender: UIBarButtonItem) {
+        let controller:FirebaseViewController =  FirebaseViewController()
+        self.navigationController?.pushViewController(controller, animated: true)
     }
     
-    @IBAction func cameraButton(_ sender: UIButton) {
-        present(pickerController, animated: true, completion: nil)
+    @objc func cameraButton(_ sender: UIButton) {
+        PHPhotoLibrary.execute(controller: self, onAccessHasBeenGranted: {
+            self.present(self.pickerController, animated: true, completion: nil)
+        })
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -64,4 +75,110 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(error)
         }
     }
+    
+    func addUiElements() {
+        imageView = UIImageView()
+        imageView.image = UIImage(named: "welcome.jpg")
+        
+        camera = UIButton();
+        camera?.setTitle("PHOTOS", for: UIControl.State.normal);
+        camera?.addTarget(self, action: #selector(ViewController.cameraButton), for: UIControl.Event.touchUpInside);
+        camera?.setTitleColor(UIColor.black, for: UIControl.State.normal);
+        camera?.backgroundColor=UIColor.gray;
+        
+        nextPage = UIButton();
+        nextPage?.setTitle("TEXT RECOGNIZER", for: UIControl.State.normal);
+        nextPage?.addTarget(self, action: #selector(ViewController.textRecognizer), for: UIControl.Event.touchUpInside);
+        nextPage?.setTitleColor(UIColor.black, for: UIControl.State.normal);
+        nextPage?.backgroundColor=UIColor.gray;
+        
+        objectName = UILabel();
+        objectName.textAlignment = NSTextAlignment.center;
+        objectName.text = "WELCOME PLEASE CHOOSE IMAGE"
+        
+        
+        self.view.addSubview(imageView);
+        self.view.addSubview(camera);
+        self.view.addSubview(nextPage);
+        self.view.addSubview(objectName);
+    }
+    
+    func observeOrientation() {
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications();
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.rotation),
+                                               name: UIDevice.orientationDidChangeNotification, object: nil);
+    }
+    
+    func setFrames() {
+        camera?.frame = CGRect(x: 0, y: (self.view.bounds.size.height/10),
+                              width: self.view.bounds.size.width,
+                              height: (self.view.bounds.size.height/10));
+        imageView?.frame = CGRect(x: 0, y: (2*(5+self.view.bounds.size.height/10)),
+                              width: self.view.bounds.size.width,
+                              height: (5*(self.view.bounds.size.height/10)));
+        objectName?.frame = CGRect(x: 0, y: (7*(5+self.view.bounds.size.height/10)),
+                                    width: self.view.bounds.size.width,
+                                    height: (self.view.bounds.size.height/10));
+        nextPage?.frame = CGRect(x: 0, y: (8*(5+self.view.bounds.size.height/10)),
+                                width: self.view.bounds.size.width,
+                                height: (self.view.bounds.size.height/10));
+    }
+    
+    @objc func rotation(sender: UIButton) {
+        setFrames();
+    }
 }
+
+public extension PHPhotoLibrary {
+
+   static func execute(controller: UIViewController,
+                       onAccessHasBeenGranted: @escaping () -> Void,
+                       onAccessHasBeenDenied: (() -> Void)? = nil) {
+
+      let onDeniedOrRestricted = onAccessHasBeenDenied ?? {
+        DispatchQueue.main.async{
+         let alert = UIAlertController(
+            title: "We were unable to load your album groups. Sorry!",
+            message: "You can enable access in Privacy Settings",
+            preferredStyle: .alert)
+         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+         alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
+            if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+               UIApplication.shared.open(settingsURL)
+            }
+         }))
+         controller.present(alert, animated: true)
+        }
+      }
+
+      let status = PHPhotoLibrary.authorizationStatus()
+      switch status {
+      case .notDetermined:
+         onNotDetermined(onDeniedOrRestricted, onAccessHasBeenGranted)
+      case .denied, .restricted:
+         onDeniedOrRestricted()
+      case .authorized:
+         onAccessHasBeenGranted()
+      @unknown default:
+         fatalError("PHPhotoLibrary::execute - \"Unknown case\"")
+      }
+   }
+
+}
+
+private func onNotDetermined(_ onDeniedOrRestricted: @escaping (()->Void), _ onAuthorized: @escaping (()->Void)) {
+   PHPhotoLibrary.requestAuthorization({ status in
+      switch status {
+      case .notDetermined:
+         onNotDetermined(onDeniedOrRestricted, onAuthorized)
+      case .denied, .restricted:
+         onDeniedOrRestricted()
+      case .authorized:
+         onAuthorized()
+      @unknown default:
+         fatalError("PHPhotoLibrary::execute - \"Unknown case\"")
+      }
+   })
+}
+
+
